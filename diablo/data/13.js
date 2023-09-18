e1ef9c65dc10d422125ae361343fe6a8b91fc4ac
deryabal <144775006+deryabal@users.noreply.github.com>
2023-09-18 14:49:19 +0300

    Add files via upload

---
+++ diablo/data/13.js
@@ -0,0 +1,37 @@
+/*
+progress bar
+*/
+
+<!--
+function Load(s,KacPx){
+	var tt = document.getElementById(s);
+	var MevcutGenislik = tt.offsetWidth;
+	/* mevcut geniþliði alýyoruz */
+	
+	tt.style.width=MevcutGenislik + KacPx ;
+	/* geniþlik ayarlayan satýr */
+	
+	tt.innerHTML = '% ' + (MevcutGenislik + KacPx);
+	/* geniþliði yazdýran satýr */
+	
+	/* %100 den yukarý çýkmamasý için */
+	if ((MevcutGenislik + KacPx)> 100){
+		document.location = "lvl13i_a210.html"
+	}//end if
+}//end function
+//-->
+
+/*
+linklerde ki çevçeveyi kaldýr (maxthon)
+*/
+
+<!--
+function RemoveDot()
+{
+for (a in document.links) document.links[a].onfocus = document.links[a].blur;
+}
+if (document.all)
+{
+document.onmousedown = RemoveDot;
+}
+//-->
